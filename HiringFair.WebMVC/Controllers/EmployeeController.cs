02241244f94cd68487395261a76d6ca8abdd6d39
Adam <awolanin6611@gmail.com>
2019-04-04 15:46:59 -0400

    index list

--- HiringFair.WebMVC/Controllers/EmployeeController.cs
+++ HiringFair.WebMVC/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using HiringFair.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,11 +7,13 @@ using System.Web.Mvc;
 
 namespace HiringFair.WebMVC.Controllers
 {
+    [Authorize]
     public class EmployeeController : Controller
     {
         // GET: Employee
         public ActionResult Index()
         {
+            var model = new EmployeeListItem[0];
             return View();
         }
     }
