8553d06aefbc9578bb2d0ce7e72ccbb5c7f755a1
Adam <awolanin6611@gmail.com>
2019-04-04 15:12:45 -0400

    second

---
+++ HiringFair.Data/Employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HiringFair.Data
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Age { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public string Race { get; set; }
+    }
+}
